10e11b4fe19928b31e5b6518415c20acbe513819
Arthur Martins <arthurmartins@mac.home>
2025-10-04 11:53:44 +0100

    Alteração do aplicattion-docker para acrescentar o grpc + alteração do pom para rodar via compose + optimização do Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,20 @@
+# ---------- STAGE 1: BUILD ----------
 FROM maven:3.9.6-eclipse-temurin-21 AS builder
+
 WORKDIR /app
+
 COPY pom.xml .
 RUN mvn dependency:go-offline
 
-COPY . .
-RUN mvn clean install
+COPY src ./src
 
+RUN mvn clean package -DskipTests
+
+# ---------- STAGE 2: RUNTIME ----------
 FROM eclipse-temurin:21-jre
+
 WORKDIR /app
 EXPOSE 8080 9090
+
 COPY --from=builder /app/target/*.jar usuarios.jar
 ENTRYPOINT ["java", "-jar", "usuarios.jar"]
--- src/main/java/br/com/clinicafiap/ClinicaFiapUsuariosApplication.java
+++ src/main/java/br/com/clinicafiap/ClinicaFiapUsuariosApplication.java
@@ -1,10 +1,11 @@
 package br.com.clinicafiap;
 
+import net.devh.boot.grpc.server.autoconfigure.GrpcServerSecurityAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {GrpcServerSecurityAutoConfiguration.class})
 public class ClinicaFiapUsuariosApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ClinicaFiapUsuariosApplication.class, args);

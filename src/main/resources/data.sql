e706ed1bac664aa01d44ac39724980c3efb6c256
Arthur Martins <arthur.mezaonik@gmail.com>
2025-10-05 20:51:41 +0100

    Alteração do data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,53 +7,53 @@ insert into perfil (nome) values ('Paciente');
 ---- usuários ---- 
 
 insert into usuario (id, nome, email, senha, ativo, id_perfil)
-values ('b249b761-acdd-46d1-8c1c-7517b1ffdc3a', 'João', 'joao@clinicafiap.com', '$2a$10$/5uvJC/dbYoclIf8BwxbSezUfN0cfKRWHR5LvdkRPPnpNRmxJwu6.', 1, 1);
+values ('b249b761-acdd-46d1-8c1c-7517b1ffdc3a', 'João', 'posfiapteam35@gmail.com', '$2a$10$/5uvJC/dbYoclIf8BwxbSezUfN0cfKRWHR5LvdkRPPnpNRmxJwu6.', 1, 1);
     
 insert into usuario (id, nome, email, senha, ativo, id_perfil)
-values ('516dfc3e-cc76-40f1-80a0-a73243620fbf', 'Pedro', 'pedro@clinicafiap.com', '$2a$10$/5uvJC/dbYoclIf8BwxbSezUfN0cfKRWHR5LvdkRPPnpNRmxJwu6.', 1, 1);
+values ('516dfc3e-cc76-40f1-80a0-a73243620fbf', 'Pedro', 'posfiapteam35@gmail.com', '$2a$10$/5uvJC/dbYoclIf8BwxbSezUfN0cfKRWHR5LvdkRPPnpNRmxJwu6.', 1, 1);
 
 insert into usuario (id, nome, email, senha, ativo, id_perfil)
-values ('a372d3ae-3fae-4f95-94a5-ea439fb493d0', 'Thiago', 'thiago@clinicafiap.com', '$2a$10$/5uvJC/dbYoclIf8BwxbSezUfN0cfKRWHR5LvdkRPPnpNRmxJwu6.', 1, 1);
+values ('a372d3ae-3fae-4f95-94a5-ea439fb493d0', 'Thiago', 'posfiapteam35@gmail.com', '$2a$10$/5uvJC/dbYoclIf8BwxbSezUfN0cfKRWHR5LvdkRPPnpNRmxJwu6.', 1, 1);
 
 insert into usuario (id, nome, email, senha, ativo, id_perfil)
-values ('976ff2c7-0ad5-4d18-88c0-719e1939182d', 'Lucas', 'lucas@clinicafiap.com', '$2a$10$/5uvJC/dbYoclIf8BwxbSezUfN0cfKRWHR5LvdkRPPnpNRmxJwu6.', 0, 1);
+values ('976ff2c7-0ad5-4d18-88c0-719e1939182d', 'Lucas', 'posfiapteam35@gmail.com', '$2a$10$/5uvJC/dbYoclIf8BwxbSezUfN0cfKRWHR5LvdkRPPnpNRmxJwu6.', 0, 1);
 
 insert into usuario (id, nome, email, senha, ativo, id_perfil)
-values ('162f50f4-7a92-403e-ac5f-0dacbe80935d', 'Livia', 'livia@clinicafiap.com', '$2a$10$/5uvJC/dbYoclIf8BwxbSezUfN0cfKRWHR5LvdkRPPnpNRmxJwu6.', 0, 1);
+values ('162f50f4-7a92-403e-ac5f-0dacbe80935d', 'Livia', 'posfiapteam35@gmail.com', '$2a$10$/5uvJC/dbYoclIf8BwxbSezUfN0cfKRWHR5LvdkRPPnpNRmxJwu6.', 0, 1);
 
 insert into usuario (id, nome, email, senha, ativo, id_perfil)
-values ('0e8b5f0a-6c8f-4510-a8c6-37dc96ce7ca0', 'Raquel', 'raquel@clinicafiap.com', '$2a$10$/5uvJC/dbYoclIf8BwxbSezUfN0cfKRWHR5LvdkRPPnpNRmxJwu6.', 0, 1);
+values ('0e8b5f0a-6c8f-4510-a8c6-37dc96ce7ca0', 'Raquel', 'posfiapteam35@gmail.com', '$2a$10$/5uvJC/dbYoclIf8BwxbSezUfN0cfKRWHR5LvdkRPPnpNRmxJwu6.', 0, 1);
 
 insert into usuario (id, nome, email, senha, ativo, id_perfil)
-values ('72b855c9-10bc-444c-9862-be4d733b462b', 'Matheus', 'matheus@clinicafiap.com', '$2a$10$/5uvJC/dbYoclIf8BwxbSezUfN0cfKRWHR5LvdkRPPnpNRmxJwu6.', 1, 1);
+values ('72b855c9-10bc-444c-9862-be4d733b462b', 'Matheus', 'posfiapteam35@gmail.com', '$2a$10$/5uvJC/dbYoclIf8BwxbSezUfN0cfKRWHR5LvdkRPPnpNRmxJwu6.', 1, 1);
 
 insert into usuario (id, nome, email, senha, ativo, id_perfil)
-values ('f6e9252c-a5a7-433f-85ad-fa2c359009d0', 'Arthur', 'arthur@clinicafiap.com', '$2a$10$/5uvJC/dbYoclIf8BwxbSezUfN0cfKRWHR5LvdkRPPnpNRmxJwu6.', 1, 1);
+values ('f6e9252c-a5a7-433f-85ad-fa2c359009d0', 'Arthur', 'posfiapteam35@gmail.com', '$2a$10$/5uvJC/dbYoclIf8BwxbSezUfN0cfKRWHR5LvdkRPPnpNRmxJwu6.', 1, 1);
 
 insert into usuario (id, nome, email, senha, ativo, id_perfil)
-values ('6eb45036-74a6-427d-8c87-f902b1be31df', 'Jedaias', 'jedaias@clinicafiap.com', '$2a$10$/5uvJC/dbYoclIf8BwxbSezUfN0cfKRWHR5LvdkRPPnpNRmxJwu6.', 1, 1);
+values ('6eb45036-74a6-427d-8c87-f902b1be31df', 'Jedaias', 'posfiapteam35@gmail.com', '$2a$10$/5uvJC/dbYoclIf8BwxbSezUfN0cfKRWHR5LvdkRPPnpNRmxJwu6.', 1, 1);
 
 insert into usuario (id, nome, email, senha, ativo, id_perfil)
-values ('41357ef8-67ba-4a81-83e2-e640671caffd', 'Ana', 'ana@clinicafiap.com', '$2a$10$/5uvJC/dbYoclIf8BwxbSezUfN0cfKRWHR5LvdkRPPnpNRmxJwu6.', 1, 2);
+values ('41357ef8-67ba-4a81-83e2-e640671caffd', 'Ana', 'posfiapteam35@gmail.com', '$2a$10$/5uvJC/dbYoclIf8BwxbSezUfN0cfKRWHR5LvdkRPPnpNRmxJwu6.', 1, 2);
 
 insert into usuario (id, nome, email, senha, ativo, id_perfil)
-values ('ff262d1a-1786-4d3d-b4b9-1e6029692b36', 'Maria', 'maria@clinicafiap.com', '$2a$10$/5uvJC/dbYoclIf8BwxbSezUfN0cfKRWHR5LvdkRPPnpNRmxJwu6.', 0, 2);
+values ('ff262d1a-1786-4d3d-b4b9-1e6029692b36', 'Maria', 'posfiapteam35@gmail.com', '$2a$10$/5uvJC/dbYoclIf8BwxbSezUfN0cfKRWHR5LvdkRPPnpNRmxJwu6.', 0, 2);
 
 insert into usuario (id, nome, email, senha, ativo, id_perfil)
-values ('322ba8f2-becf-4499-825e-edb9086432b0', 'Paulo', 'paulo@clinicafiap.com', '$2a$10$/5uvJC/dbYoclIf8BwxbSezUfN0cfKRWHR5LvdkRPPnpNRmxJwu6.', 1, 2);
+values ('322ba8f2-becf-4499-825e-edb9086432b0', 'Paulo', 'posfiapteam35@gmail.com', '$2a$10$/5uvJC/dbYoclIf8BwxbSezUfN0cfKRWHR5LvdkRPPnpNRmxJwu6.', 1, 2);
 
 insert into usuario (id, nome, email, senha, ativo, id_perfil)
-values ('a4d6aa30-c9f5-4a11-b6af-ab7b10248449', 'Juliana', 'juliana@clinicafiap.com', '$2a$10$/5uvJC/dbYoclIf8BwxbSezUfN0cfKRWHR5LvdkRPPnpNRmxJwu6.', 1, 2);
+values ('a4d6aa30-c9f5-4a11-b6af-ab7b10248449', 'Juliana', 'posfiapteam35@gmail.com', '$2a$10$/5uvJC/dbYoclIf8BwxbSezUfN0cfKRWHR5LvdkRPPnpNRmxJwu6.', 1, 2);
 
 insert into usuario (id, nome, email, senha, ativo, id_perfil)
-values ('81c30cd3-7755-4aa8-b6b3-30d07a6ca3d2', 'Julio', 'ana@email.com', '$2a$10$/5uvJC/dbYoclIf8BwxbSezUfN0cfKRWHR5LvdkRPPnpNRmxJwu6.', 1, 3);
+values ('81c30cd3-7755-4aa8-b6b3-30d07a6ca3d2', 'Julio', 'posfiapteam35@gmail.com', '$2a$10$/5uvJC/dbYoclIf8BwxbSezUfN0cfKRWHR5LvdkRPPnpNRmxJwu6.', 1, 3);
 
 insert into usuario (id, nome, email, senha, ativo, id_perfil)
-values ('deaffe6c-3668-4352-b90c-df6f49835fd2', 'Juliana', 'maria@email.com', '$2a$10$/5uvJC/dbYoclIf8BwxbSezUfN0cfKRWHR5LvdkRPPnpNRmxJwu6.', 0, 3);
+values ('deaffe6c-3668-4352-b90c-df6f49835fd2', 'Juliana', 'posfiapteam35@gmail.com', '$2a$10$/5uvJC/dbYoclIf8BwxbSezUfN0cfKRWHR5LvdkRPPnpNRmxJwu6.', 0, 3);
 
 insert into usuario (id, nome, email, senha, ativo, id_perfil)
-values ('1f19a219-1bc5-44c7-af55-161f39d1f335', 'Roberto', 'paulo@email.com', '$2a$10$/5uvJC/dbYoclIf8BwxbSezUfN0cfKRWHR5LvdkRPPnpNRmxJwu6.', 1, 3);
+values ('1f19a219-1bc5-44c7-af55-161f39d1f335', 'Roberto', 'posfiapteam35@gmail.com', '$2a$10$/5uvJC/dbYoclIf8BwxbSezUfN0cfKRWHR5LvdkRPPnpNRmxJwu6.', 1, 3);
 
 insert into usuario (id, nome, email, senha, ativo, id_perfil)
-values ('735ed011-f072-40a8-bc54-2dd4dd4d52a8', 'Maria', 'maria@email.com', '$2a$10$/5uvJC/dbYoclIf8BwxbSezUfN0cfKRWHR5LvdkRPPnpNRmxJwu6.', 1, 3);
+values ('735ed011-f072-40a8-bc54-2dd4dd4d52a8', 'Maria', 'posfiapteam35@gmail.com', '$2a$10$/5uvJC/dbYoclIf8BwxbSezUfN0cfKRWHR5LvdkRPPnpNRmxJwu6.', 1, 3);
 
